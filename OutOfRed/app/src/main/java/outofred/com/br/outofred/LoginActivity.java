443a8ea2b855d1af3b6dc301ff21f319d9312135
Ivo Mancinelli <ivo_mancinelli@hotmail.com>
2017-09-28 21:02:19 -0300

    Layout terminado

--- OutOfRed/app/src/main/java/outofred/com/br/outofred/LoginActivity.java
+++ OutOfRed/app/src/main/java/outofred/com/br/outofred/LoginActivity.java
@@ -1,7 +1,10 @@
 package outofred.com.br.outofred;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login = (Button) findViewById(R.id.login_button_entrar);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(intent);
+            }
+        });
     }
 }
