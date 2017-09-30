e319215e5386a7d7afc7e586e1cb1a39c037aab5
Ivo Mancinelli <ivo_mancinelli@hotmail.com>
2017-09-29 21:23:59 -0300

    layout tela success

--- OutOfRed/app/src/main/java/outofred/com/br/outofred/MainActivity.java
+++ OutOfRed/app/src/main/java/outofred/com/br/outofred/MainActivity.java
@@ -1,8 +1,11 @@
 package outofred.com.br.outofred;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner_parcelas);
-// Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.parcelas_array, R.layout.spinner_item);
-// Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+
+        Button pagar = (Button) findViewById(R.id.main_button_pagar);
+        pagar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SuccessActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
